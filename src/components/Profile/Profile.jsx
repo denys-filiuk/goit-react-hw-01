dffe59beb3689c84db4d4475af0f56ed52f662df
denys-filiuk <filiuk_dp@ukr.net>
2025-04-24 13:35:00 +0300

    create 1 task js

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,30 @@
+export default function Profile({ name, tag, location, image, stats: {followers, views, likes} }) {
+    return (
+    <div>
+        <div>
+            <img
+                src={image}
+                alt={name}
+            />
+            <p>{name}</p>
+            <p>@{tag}</p>
+            <p>{location}</p>
+        </div>
+
+        <ul>
+            <li>
+                <span>Followers</span>
+                <span>{followers}</span>
+            </li>
+            <li>
+                <span>Wiews</span>
+                <span>{views}</span>
+            </li>
+            <li>
+                <span>Likes</span>
+                <span>{likes}</span>
+            </li>
+        </ul>
+    </div>
+    )
+}
